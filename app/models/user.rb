9000686ee064b40a030206130160810f1cb064d4
rkyankya <rkyankya@gmail.com>
2020-04-21 21:32:14 +0300

    Manually add role to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,20 @@
 class User < ApplicationRecord
   rolify
   resourcify
+  # before_create :set_default_role, if: :new_record?
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :roles, presence: false
+
+  enum role: %i[staff normal admin]
+
+  private
+
+  # def set_default_role
+  #   self.role ||= :normal
+  # end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +7,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |n|
+  email = "foo#{n + 1}@bar.com"
+  password = 'foobar'
+  User.create(email: email, password: password)
+end
+
+5.times do |n|
+  email = Faker::Name.name
+  username = "foo#{n + 1}"
+  User.create!(name: name,
+               username: username)
+end
